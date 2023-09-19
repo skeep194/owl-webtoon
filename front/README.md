51aa6c73bed15f22669aa6d5c4f31075d6f98dbb
Kadrick <kbk2581553@gmail.com>
2023-09-19 17:49:51 +0900

    init react project: hello, world!

---
+++ front/README.md
@@ -0,0 +1,16 @@
+---
+title: owl-webtoon front section readme
+---
+
+## Rules
+
+## Packages
+
+-   [Chakra UI](https://chakra-ui.com)
+-   []
+
+## React
+
+### Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ front/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Center, Container, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Center>
+      <Container width={"container.md"}>
+        <Center>
+          <Text>Hello, world!</Text>
+        </Center>
+      </Container>
+    </Center>
+  );
+}
+
+export default App;
